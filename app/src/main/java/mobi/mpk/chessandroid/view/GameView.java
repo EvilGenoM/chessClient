698c8e6cba0ab1014ca8dd62e2227634a09450d5
EvilGenoM <evmezentsev@gmail.com>
2017-10-05 16:54:51 +0300

    Добавлено view для игры

--- app/src/main/java/mobi/mpk/chessandroid/view/GameView.java
+++ app/src/main/java/mobi/mpk/chessandroid/view/GameView.java
@@ -2,42 +2,38 @@ package mobi.mpk.chessandroid.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
-import java.util.List;
-import java.util.logging.Logger;
-
-import mobi.mpk.chessandroid.domain.Board;
+import mobi.mpk.chessandroid.R;
+import mobi.mpk.chessandroid.view.figure.FigureView;
 
 /**
- * Created by evgen on 03.10.17.
+ * Created by evgen on 04.10.17.
  */
 
 public class GameView extends View {
 
-    private Board board;
-    private BoardView boardView;
-    private StrokeView strokeView;
+    private final int INDENT_SIDE = 50;
 
+    private BoardView boardView;
+
+    private Paint paint;
+    private Context context;
     private Canvas canvas;
 
-    private int width;
-    private int height;
-
-    public GameView(Context context, AttributeSet attrs) {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        board = new Board();
-        boardView = new BoardView(context, board);
+        this.context = context;
     }
 
     @Override
@@ -46,19 +42,46 @@ public class GameView extends View {
         int x = (int) event.getX();
         int y = (int) event.getY();
 
-        strokeView = new StrokeView(canvas, boardView);
-        strokeView.handle(x, y);
+        canvas.drawText("Succes", 50, 50, paint);
 
         return true;
     }
 
     @Override
-    protected void onDraw(Canvas canvas){
+    public void onDraw(Canvas canvas){
         super.onDraw(canvas);
-
         this.canvas = canvas;
 
-        boardView.onDraw(canvas);
+        initBoard();
+
+        boardView.onDraw(canvas, context);
+    }
+
+    private void initBoard() {
+
+        int width = this.getWidth();
+        int height = this.getHeight();
+
+        int left, right, top, bottom;
+
+        if(width < height) {
+
+            left = INDENT_SIDE;
+            right = width - INDENT_SIDE;
+
+            top = (height / 2) - (right - left) / 2;
+            bottom = top + (right - left);
+
+        } else {
+
+            top = INDENT_SIDE;
+            bottom = height - INDENT_SIDE;
+
+            left = (width / 2) - (bottom - top) / 2;
+            right = left + (bottom - top);
+
+        }
+        boardView = new BoardView(left, top, right, bottom, this);
 
     }
 
