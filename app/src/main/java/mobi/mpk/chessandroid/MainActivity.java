be4133ba9d10ea92f6df91f6a39f395a31c44ed9
EvilGenoM <evmezentsev@gmail.com>
2017-10-05 16:56:43 +0300

    Refactoring project

--- app/src/main/java/mobi/mpk/chessandroid/MainActivity.java
+++ app/src/main/java/mobi/mpk/chessandroid/MainActivity.java
@@ -1,14 +1,21 @@
 package mobi.mpk.chessandroid;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Display display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
     }
