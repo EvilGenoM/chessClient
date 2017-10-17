a3b666d57cca2073383df2db3a92a8eea4ddd418
EvilGenoM <evmezentsev@gmail.com>
2017-10-17 13:47:39 +0300

    Добавлено представление настроек языка

---
+++ app/src/main/java/mobi/mpk/chessandroid/ui/settings/LanguageSettingsActivity.java
@@ -0,0 +1,18 @@
+package mobi.mpk.chessandroid.ui.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mobi.mpk.chessandroid.R;
+
+public class LanguageSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_language_settings);
+
+    }
+}
