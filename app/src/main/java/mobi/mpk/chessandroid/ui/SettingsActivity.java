a723df5826900ccf26d0e62045d47fb760f3aadd
EvilGenoM <evmezentsev@gmail.com>
2017-10-18 22:36:58 +0300

    Реализованы настройки с помощью Preference

--- app/src/main/java/mobi/mpk/chessandroid/ui/SettingsActivity.java
+++ app/src/main/java/mobi/mpk/chessandroid/ui/SettingsActivity.java
@@ -1,24 +1,18 @@
 package mobi.mpk.chessandroid.ui;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.preference.PreferenceActivity;
 
 import mobi.mpk.chessandroid.R;
 
-public class SettingsActivity extends ListActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_settings);
-
-        String[] options = getResources().getStringArray(R.array.options);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_settings, R.id.list_content, options);
-        setListAdapter(adapter);
+        addPreferencesFromResource(R.xml.settings);
         
     }
 
