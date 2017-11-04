4124e5de49ec3591bb69d5cef7b403be33ba8067
EvilGenoM <evmezentsev@gmail.com>
2017-11-04 23:58:36 +0300

    Рефакторинг класса Settings

---
+++ app/src/main/java/mobi/mpk/chessandroid/settings/SoundSettings.java
@@ -0,0 +1,22 @@
+package mobi.mpk.chessandroid.settings;
+
+
+public class SoundSettings {
+
+    private boolean volume;
+
+    public SoundSettings() {
+
+        volume = true;
+
+    }
+
+    public boolean isVolume() {
+        return volume;
+    }
+
+    public void setVolume(boolean volume) {
+        this.volume = volume;
+    }
+
+}
